c07b3fd375ddcaad6ba80e6eb0f549b9dd5f2655
Chongguang LIU <lcg31439@gmail.com>
2019-07-10 11:15:28 +0200

    add magain id information to product case class (#6)

--- solution/src/main/scala/com/carrefour/App.scala
+++ solution/src/main/scala/com/carrefour/App.scala
@@ -1,15 +1,14 @@
 package com.carrefour
 
 import Transaction.getTransactions
-import Product.getProducts
-import Magasin.MagasinIds
+import Product.getAllProductsOfDay
 
 object App {
 
   def main(args: Array[String]): Unit = {
     println("Hello")
     val transactions: List[Transaction] = getTransactions("20170514")
-    val products: List[Product] = getProducts(MagasinIds.head, "20170514")
+    val products: List[Product] = getAllProductsOfDay("20170514")
     transactions.take(10).foreach(t => println(t))
     products.take(10).foreach(t => println(t))
   }
--- solution/src/main/scala/com/carrefour/Product.scala
+++ solution/src/main/scala/com/carrefour/Product.scala
@@ -4,17 +4,19 @@ import java.util.logging.Logger
 
 import scala.io.Source
 
-case class Product(produit: Long, prix: Double, date: String)
+case class Product(produit: Long, prix: Double, date: String, magasin: String)
 
 object Product {
 
-  def getProducts(magasin: String, date: String): List[Product] =
-    Source.fromResource(s"reference_prod-$magasin" + s"_$date.data").getLines.flatMap(parseProduct(date)).toList
+  def getAllProductsOfDay(date: String): List[Product] = Magasin.MagasinIds.flatMap(m => getProducts(m, date))
 
-  def parseProduct(date: String)(s: String): Option[Product] = {
+  def getProducts(magasin: String, date: String): List[Product] =
+    Source.fromResource(s"reference_prod-$magasin" + s"_$date.data").getLines.flatMap(parseProduct(date, magasin)).toList
+
+  def parseProduct(date: String, magasin: String)(s: String): Option[Product] = {
     val sList = s.split('|').map(_.trim)
     try {
-      Some(Product(sList(0).toLong, sList(1).toDouble, date))
+      Some(Product(sList(0).toLong, sList(1).toDouble, date, magasin))
     } catch {
       case e: Exception =>
         Logger.getAnonymousLogger.warning(s"Caught the following exception while parsing the product line $s: $e.")
